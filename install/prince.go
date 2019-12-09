86d0676683117301cd1b4cf298b9dce21ba07d57
xuan200922702 <200922702@qq.com>
2019-12-09 11:44:07 +0800

    内测版

--- install/prince.go
+++ install/prince.go
@@ -48,7 +48,7 @@ func (p *PrinceInstaller)InstallMaster0(){
 	cmd := fmt.Sprintf("echo %s %s >> /etc/hosts", IpFormat(Masters[0]), ApiServer)
 	Cmdout(Masters[0], cmd)
 	cmd = p.Command(Version,InitMaster)
-	output:=Cmdout(Masters[0],cmd)
+	output:=Cmd(Masters[0],cmd)
 	if output == nil {
 		logger.Error("[%s]kubernetes install is error.please clean and uninstall.", Masters[0])
 		os.Exit(1)
